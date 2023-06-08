f4e7fededf8a9227b81569b7098044b0feb45dfc
TheodorWulff <104894109+TheodorWulff@users.noreply.github.com>
2023-06-08 15:25:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.6.0"
-    }
-  }
-}
-
 locals {
   pip = {
       enabled = var.pip_config.enabled

d6668b360edbad5dfd33608df4823f6bfa143a39
christian.hartmann@qbeyond.de <christian.hartmann@qbeyond.de>
2023-06-13 11:54:24 +0200

    added data_disk tf to enable the creation of multiple disks

---
+++ data_disk.tf
@@ -0,0 +1,30 @@
+resource "azurerm_managed_disk" "data_disk" {
+  count                = length(var.data_disks)
+  name                 = "${var.vm_config.hostname}-datadisk-${format("%02d", count.index)}"
+  location             = local.vm.location
+  resource_group_name  = var.resource_group.name
+  storage_account_type = var.data_disks[count.index].storage_account_type
+  create_option        = var.data_disks[count.index].create_option
+  disk_size_gb         = var.data_disks[count.index].disk_size_gb
+  zone                 = var.vm_config.zone == null ? null : [var.vm_config.zone]
+
+  lifecycle {
+  ignore_changes = [
+    tags
+  ]
+  }
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "data_disk" {
+  count              = length(var.data_disks)
+  managed_disk_id    = azurerm_managed_disk.data_disk[count.index].id
+  virtual_machine_id = azurerm_windows_virtual_machine.this.id
+  lun                = count.index
+  caching            = var.data_disks.caching
+
+  lifecycle {
+  ignore_changes = [
+    tags
+  ]
+  }
+}

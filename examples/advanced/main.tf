15df69c26c52087983b19dabbb73d60868a67b6d
JesStruk <jesper.struck@qbeyond.de>
2023-07-12 07:05:21 +0000

    added examples and worked on minor changes

---
+++ examples/advanced/main.tf
@@ -0,0 +1,96 @@
+provider "azurerm" {
+  features {}
+}
+
+module "virtual_machine" {
+  source = "../.."
+  public_ip_config = {
+    enabled = true
+    allocation_method = "Static"
+  }
+  nic_config = {
+    private_ip = "10.0.0.16"
+    dns_servers = [ "10.0.0.10", "10.0.0.11" ]
+    nsg_id = azurerm_network_security_group.this.id
+    nsg_link = true
+  }
+  virtual_machine_config = {
+    hostname = "CUSTAPP007"
+    size = "Standard_D2_v5"
+    os_sku = "2022-datacenter-g2"
+    location = azurerm_resource_group.this.location
+    availability_set_id = azurerm_availability_set.this.id
+    os_version = "latest"
+    admin_username = "loc_admin"
+    os_disk_caching = "ReadWrite"
+    os_disk_storage_type = "StandardSSD_LRS"
+    os_disk_size_gb = 128
+    tags = {
+      "Environment" = "Prod" 
+    }
+    write_accelerator_enabled = false
+  }
+  admin_password = "H3ll0W0rld!"
+  resource_group_name = azurerm_resource_group.this.name
+  subnet = azurerm_subnet.this
+  data_disks = {
+    1 = {
+        name = local.managed_disk_name
+        caching = "ReadWrite"
+        disk_size_gb = 64
+        create_option = "Empty"
+        storage_account_type = "Standard_LRS"
+        write_accelerator_enabled = false
+    }
+  }
+
+  name_overrides = {
+    nic = local.nic
+    nic_ip_config = local.nic_ip_config
+    public_ip = local.public_ip
+    virtual_machine = local.virtual_machine
+  }
+}
+
+resource "azurerm_resource_group" "this" {
+  name = local.resource_group_name
+  location = local.location
+}
+
+resource "azurerm_virtual_network" "this" {
+  name = local.virtual_network_name
+  address_space = [ "10.0.0.0/24" ]
+  location = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "this" {
+  name = local.subnet_name
+  resource_group_name = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes = [ "10.0.0.0/24" ]
+}
+
+resource "azurerm_availability_set" "this" {
+  name = local.availability_set_name
+  location = local.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_network_security_group" "this" {
+  name = local.nsg_name
+  location = local.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name = "example"
+    priority = 100
+    direction = "Outbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "*"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+}

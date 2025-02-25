c653fb6005ca2c072b36c29b36449a1384514fb4
QBY-MauriceBaerisch <Maurice.Baerisch@qbeyond.de>
2025-02-25 14:05:35 +0100

    fix: fix validation for vmconfig.zone

--- variables.tf
+++ variables.tf
@@ -89,7 +89,7 @@ variable "virtual_machine_config" {
     error_message = "os_disk_write_accelerator_enabled, can only be activated on Premium disks and caching deactivated."
   }
   validation {
-    condition     = var.virtual_machine_config.zone == null || var.virtual_machine_config.zone == 1 || var.virtual_machine_config.zone == 2 || var.virtual_machine_config.zone == 3
+    condition     = var.virtual_machine_config.zone == null || var.virtual_machine_config.zone == "1" || var.virtual_machine_config.zone == "2" || var.virtual_machine_config.zone == "3"
     error_message = "Zone, can only be empty, 1, 2 or 3."
   }
   description = <<-DOC

863ae44fc5f58d8160b6170b9f2868951e8b8037
madir <marcel.dirks@qbeyond.de>
2026-07-08 17:18:37 +0200

    Fixed error

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "this" {
   count               = var.public_ip_config.enabled ? 1 : 0
-  name                = var.name_overrides.resource_group_name_public_ip != null ? var.name_overrides.resource_group_name_public_ip : var.resource_group_name
-  resource_group_name = var.resource_group_name
+  name                = local.public_ip.name
+  resource_group_name = var.name_overrides.resource_group_name_public_ip != null ? var.name_overrides.resource_group_name_public_ip : var.resource_group_name
   location            = var.virtual_machine_config.location
   allocation_method   = var.public_ip_config.allocation_method
   zones               = var.virtual_machine_config.zone != null ? [var.virtual_machine_config.zone] : null
